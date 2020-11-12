da37f0172e3cd305bedcb4503d7dca20c108af08
DanielDO93 <danieldionisio1993@gmail.com>
2020-11-12 16:38:12 -0600

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -6,36 +6,23 @@ export default {
       class: 'text-center',
     },
     {
-      name: 'Reglas ortográficas de la V',
+      name: 'PQRS',
       icon: 'icon-tag',
      
     },
-
-   
     {
-      name: 'Evaluación 1',
-      icon: 'icon-puzzle',
-    
+      name: 'PQRS',
+      icon: 'icon-tag',
+     
     },
+  
     {
-      name: 'Evaluación 2',
-      icon: 'icon-puzzle',
-    
-    },
-    {
-      name: 'Evaluación 3',
-      icon: 'icon-puzzle',
-    
-    },
-
-    {
-      name: 'Conclusión',
+      name: 'Despedida',
       icon: 'icon-tag',
       
     },
-
     {
-      name: 'Despedida',
+      name: 'Evaluación del curso',
       icon: 'icon-tag',
       
     },
